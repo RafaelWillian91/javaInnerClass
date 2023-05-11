5e917b038212c13ac7c2b2d2cc3ace7d359b7b34
rafaw <rafa.willian91@gmail.com>
2023-05-11 02:39:00 -0300

    Criando uma Regular Inner Class

---
+++ src/innerclass/regular/Operation.java
@@ -0,0 +1,28 @@
+package innerclass.regular;
+
+public class Operation {
+
+    private final  int value1;
+
+    private final int value2;
+
+    public Operation(int value1, int value2) {
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+    public int execute(){
+        Sum sum = new Sum();
+        return sum.calculate();
+    }
+
+    //A classe Sum é muito atrelada a operation, ela não tem sentido sozinha. Entao, ela pode ser uma innerClass.
+
+    //**** Regular innerClass **** privada. A innerClass tem acesso aos atributos da sua alterCLass, ou seja, a classe externa a
+    //innerClass
+    private class Sum {
+
+        public int calculate() {
+            return value1 + value2;//Existe a visibilidade dos atributos da alterClass
+        }
+    }
+}
---
+++ src/innerclass/regular/RegularInnerClassapp1.java
@@ -0,0 +1,11 @@
+package innerclass.regular;
+
+public class RegularInnerClassapp1 {
+
+    public static void main(String[] args) {
+
+        Operation op = new Operation(10, 23);
+        System.out.println(op.execute());
+
+    }
+}
